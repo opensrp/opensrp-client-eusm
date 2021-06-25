8af2b4d6b2ad94b0bcf36116989739e40a7910e0
bennsimon <botieno@ona.io>
2021-06-25 13:02:02 +0300

    remove condition for updating jurisdictions

--- opensrp-eusm/src/main/java/org/smartregister/eusm/activity/LoginActivity.java
+++ opensrp-eusm/src/main/java/org/smartregister/eusm/activity/LoginActivity.java
@@ -1,12 +1,10 @@
 package org.smartregister.eusm.activity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 
 import org.smartregister.eusm.R;
 import org.smartregister.eusm.application.EusmApplication;
 import org.smartregister.eusm.presenter.LoginPresenter;
-import org.smartregister.eusm.util.AppConstants;
 import org.smartregister.eusm.util.AppUtils;
 import org.smartregister.eusm.util.TestDataUtils;
 import org.smartregister.service.UserService;
@@ -38,18 +36,11 @@ public class LoginActivity extends BaseLoginActivity implements BaseLoginContrac
         TestDataUtils testDataUtils = new TestDataUtils();
         testDataUtils.populateTestData();
 
-        SharedPreferences allSharedPreferences = Utils.getAllSharedPreferences().getPreferences();
-        if (allSharedPreferences != null) {
-            boolean hasLoadedDistrictsFromHierarchy = allSharedPreferences.getBoolean(AppConstants.PreferenceKey.LOADED_DISTRICTS_FROM_HIERARCHY, false);
-            if (!hasLoadedDistrictsFromHierarchy) {
-                Set<String> districtsIds = AppUtils.getDistrictsFromLocationHierarchy();
-                UserService userService = EusmApplication.getInstance().context().userService();
-                Set<String> strings = userService.fetchJurisdictionIds();
-                strings.addAll(districtsIds);
-                userService.saveJurisdictionIds(strings);
-                allSharedPreferences.edit().putBoolean(AppConstants.PreferenceKey.LOADED_DISTRICTS_FROM_HIERARCHY, true).commit();
-            }
-        }
+        Set<String> districtsIds = AppUtils.getDistrictsFromLocationHierarchy();
+        UserService userService = EusmApplication.getInstance().context().userService();
+        Set<String> locations = userService.fetchJurisdictionIds();
+        locations.addAll(districtsIds);
+        userService.saveJurisdictionIds(locations);
 
         EusmApplication.getInstance().getContext().anmLocationController().evict();
 
