08c1d720a29e4d9889566112e39f80c169d6e741
Hamza Ahmed Khan <hamza.khan@venturedive.com>
2022-12-09 11:09:11 +0500

    Fix failing tests 2

--- opensrp-eusm/src/test/java/org/smartregister/eusm/presenter/EusmBaseDrawerPresenterTest.java
+++ opensrp-eusm/src/test/java/org/smartregister/eusm/presenter/EusmBaseDrawerPresenterTest.java
@@ -140,36 +140,11 @@ public class EusmBaseDrawerPresenterTest extends BaseUnitTest {
     }
 
     @Test
-    public void testGetEntireTreeShouldNotFIlterANyDistrict() {
-        String jurisdictionId = "34324";
-        String planId = UUID.randomUUID().toString();
-        PreferencesUtil preferencesUtil = mock(PreferencesUtil.class);
-        doReturn(planId).when(preferencesUtil).getCurrentPlanId();
-
-        ReflectionHelpers.setField(eusmBaseDrawerPresenter, "prefsUtil", preferencesUtil);
-
-        String strLocation = "[{\"type\":\"Feature\",\"id\":\"f3199af5-2eaf-46df-87c9-40d59606a2fb\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[49.54933,-16.08306]},\"properties\":{\"type\":\"Water Point\",\"status\":\"Active\",\"parentId\":\"8e74d042-4a71-4694-a652-bc3ba6369101\",\"name\":\"SOANIERANA IVONGO\",\"geographicLevel\":0,\"version\":0,\"AdminLevelTag\":\"Commune\"},\"serverVersion\":18479},{\"type\":\"Feature\",\"id\":\"b8a7998c-5df6-49eb-98e6-f0675db71848\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[49.58436,-16.433]},\"properties\":{\"type\":\"Water Point\",\"status\":\"Active\",\"parentId\":\"663d7935-35e7-4ccf-aaf5-6e16f2042570\",\"name\":\"FENERIVE EST\",\"geographicLevel\":0,\"version\":0,\"AdminLevelTag\":\"Commune\"},\"serverVersion\":18480}]";
-        List<Location> locations = JsonFormUtils.gson.fromJson(strLocation, new TypeToken<List<Location>>() {
-        }.getType());
-        LocationRepository spyLocationRepository = spy(DrishtiApplication.getInstance().getContext().getLocationRepository());
-        doReturn(locations).when(spyLocationRepository).getLocationsByIds(Collections.singletonList(jurisdictionId));
-
-        Context spyOpensrpContext = spy(CoreLibrary.getInstance().context());
-        doReturn(spyLocationRepository).when(spyOpensrpContext).getLocationRepository();
-
-        ReflectionHelpers.setField(DrishtiApplication.getInstance(), "context", spyOpensrpContext);
-
-
+    public void testGetEntireTreeShouldNotFIlterAnyDistrict() {
         String strFormLocation = "[{\"key\":\"Madagascar\",\"level\":\"\",\"name\":\"Madagascar\",\"nodes\":[{\"key\":\"ANALANJIROFO\",\"level\":\"\",\"name\":\"ANALANJIROFO\",\"nodes\":[{\"key\":\"FENERIVE EST\",\"level\":\"\",\"name\":\"FENERIVE EST\",\"nodes\":[]},{\"key\":\"MANANARA AVARATRA\",\"level\":\"\",\"name\":\"MANANARA AVARATRA\",\"nodes\":[]},{\"key\":\"SOANIERANA IVONGO\",\"level\":\"\",\"name\":\"SOANIERANA IVONGO\",\"nodes\":[]},{\"key\":\"TRINIDAD IVONGO\",\"level\":\"\",\"name\":\"TRINIDAD IVONGO\",\"nodes\":[]}]}]}]";
         List<FormLocation> entireTree = JsonFormUtils.gson.fromJson(strFormLocation, new TypeToken<List<FormLocation>>() {
         }.getType());
 
-        PlanDefinitionSearch planDefinitionSearch = new PlanDefinitionSearch();
-        planDefinitionSearch.setJurisdictionId(jurisdictionId);
-        PlanDefinitionSearchRepository mockPlanDefinitionSearchRepository = mock(PlanDefinitionSearchRepository.class);
-        doReturn(Collections.singletonList(planDefinitionSearch)).when(mockPlanDefinitionSearchRepository).findPlanDefinitionSearchByPlanId(eq(planId));
-
-        ReflectionHelpers.setField(EusmApplication.getInstance(), "planDefinitionSearchRepository", mockPlanDefinitionSearchRepository);
         String result = eusmBaseDrawerPresenter.getEntireTree(entireTree);
         List<FormLocation> entireTreeResult = JsonFormUtils.gson.fromJson(result, new TypeToken<List<FormLocation>>() {
         }.getType());
